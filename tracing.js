1fb73a7a97ff36a7a20e74b952221da20cc56dbd
KaylaOates <oatesk@ufl.edu>
2023-11-26 23:36:09 -0500

    changes added

--- tracing.js
+++ tracing.js
@@ -4,6 +4,7 @@ const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
 const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
 const { trace } = require("@opentelemetry/api");
+const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
 //Instrumentations
 const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
 const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
@@ -11,7 +12,18 @@ const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 const { registerInstrumentations } = require("@opentelemetry/instrumentation");
 //Exporter
 module.exports = (serviceName) => {
-   const exporter = new ConsoleSpanExporter();
+
+    const options = {
+        tags: [], // optional
+        // You can use the default UDPSender
+        host: 'localhost', // optional
+        port: 6832, // optional
+        // OR you can use the HTTPSender as follows
+        // endpoint: 'http://localhost:14268/api/traces',
+        maxPacketSize: 65000 // optional
+      }
+      const exporter = new JaegerExporter(options);
+
    const provider = new NodeTracerProvider({
        resource: new Resource({
            [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
